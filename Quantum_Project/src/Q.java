71af796b5232533b44b94cad7d7923c8ebc3e03a
Prady <Prady@Prady-PC>
2017-10-07 18:03:27 -0400

    commited to first project

---
+++ Quantum_Project/src/Q.java
@@ -0,0 +1,96 @@
+
+public class Q {
+	
+	private double alpha, beta;
+	
+	public Q(double zero, double one){
+		alpha = zero;
+		beta = one;
+	}
+	
+	public boolean getValue(){
+		return Math.random()<(Math.pow(beta, 2));
+	}
+	
+	public void NOT(){
+		double holder = alpha;
+		alpha = beta;
+		beta = holder;
+	}
+	
+	public void Hadamard(){
+		double betaPrime = (alpha-beta)/Math.sqrt(2);
+		double alphaPrime = (alpha+beta)/Math.sqrt(2);
+		alpha = alphaPrime;
+		beta = betaPrime;
+	}
+	
+	public void PauliY(){
+		alpha*=Math.sqrt(-1);
+		beta*=-Math.sqrt(-1);
+	}
+	
+	public void PauliZ(){
+		beta*=-1;
+	}
+	
+	public void Swap(Q qubit){
+		double bHold = beta;
+		double aHold = alpha;
+		this.alpha = qubit.alpha;
+		this.beta = qubit.beta;
+		qubit.alpha = aHold;
+		qubit.beta = bHold;
+	}
+	
+	public boolean Read(){
+		if(getValue()){
+			beta = 1;
+			alpha = 0;
+			return true;
+		}
+			beta = 0;
+			alpha = 1;
+			return false;
+	}
+	
+	
+	public void cNOT(Q qubit){
+			if(qubit.Read())
+				this.NOT();
+	}
+	
+	
+	public void cHadamard(Q qubit){
+		if(qubit.Read())
+			this.Hadamard();
+	}
+	
+	
+	public void cY(Q qubit){
+		if(qubit.Read())
+			this.PauliY();
+	}
+	
+	
+	public void cZ(Q qubit){
+		if(qubit.Read())
+			this.PauliZ();
+	}
+	
+	
+	public void cSwap(Q qs, Q qubit){
+		if(qubit.Read())
+			this.Swap(qs);
+	}
+	
+	public void cRead(Q qubit){
+		if(qubit.Read())
+			this.Read();
+	}
+	
+	public String toString(){
+		return String.format("%.2f|0> + %.2f|1>\n", alpha, beta);
+		}
+	
+}
---
+++ Quantum_Project/src/Q_Sim.java
@@ -0,0 +1,131 @@
+
+import java.util.Scanner;
+
+public class Q_Sim{
+
+	private static Scanner in;
+
+	public static void main(String [] args){
+			Q[] q = new Q[128];
+			int i=0;
+			int index = -1, index2 = -1, bet = 0;
+			in = new Scanner(System.in);
+			System.out.println("Welcome to Q-Sim. Please input data. Press '?' for a list of commands");
+			String str = "";
+			while(true){
+				str = in.nextLine();
+				String[] cmd = str.split(" ");
+				
+				switch(cmd[0]) {
+				
+				case "?":
+					System.out.println("'new [n]'\t\t\tCreates new qubit with beta value n (1 or 0)\n"
+							+ "'pauli-x [k]'\t\t\tInverses the the qubit of index k\n"
+							+ "'pauli-y [k]'\t\t\tRotates the qubit of index k around the y-axis\n"
+							+ "'pauli-z [k]'\t\t\tRotates the qubit of index k around the z-axis\n"
+							+ "'hadamard [k]'\t\t\tRotates the qubit of index k around the (x+z)/sqrt(2) axis\n"
+							+ "'read [k]'\t\t\tUnsuperposes qubit of index k & Displays its value\n"
+							+ "'swap [k] [l]'\t\t\tSwaps value of qubits of index k & l\n"
+							+ "'check [j] [operation]'\t\tReads qubit of index j & performs operation based on outcome\n");
+					break;
+				
+				case "new":
+					bet = Integer.parseInt(cmd[1]);
+					if(bet==1||bet==0){
+						q[i++] = new Q(1-bet, bet);
+						System.out.printf("New qubit q%d = "+q[i-1], i-1);
+					}
+					break;
+				
+				case "pauli-x":
+					index = Integer.parseInt(cmd[1]);
+					q[index].NOT();
+					System.out.printf("Qubit q%d = "+q[index], index);
+					break;
+				
+				case "pauli-y":
+					index = Integer.parseInt(cmd[1]);
+					q[index].PauliY();
+					System.out.printf("Qubit q%d = "+q[index], index);
+					break;
+				
+				case "pauli-z":
+					index = Integer.parseInt(cmd[1]);
+					q[index].PauliZ();
+					System.out.printf("Qubit q%d = "+q[index], index);
+					break; 
+				
+				case "hadamard":
+					index = Integer.parseInt(cmd[1]);
+					q[index].Hadamard();
+					System.out.printf("Qubit q%d = "+q[index], index);
+					break;
+				
+				case "read":
+					index = Integer.parseInt(cmd[1]);
+					q[index].Read();
+					System.out.printf("Qubit q%d = "+q[index], index); 
+					break;
+				
+				case "swap":
+					index = Integer.parseInt(cmd[1]);
+					index2 = Integer.parseInt(cmd[2]);
+					q[index].Swap(q[index2]);
+					System.out.printf("Qubit q%d = "+q[index], index);
+					System.out.printf("Qubit q%d = "+q[index2], index2);
+					break;
+					
+				case "check": int c = Integer.parseInt(cmd[1]);
+					q[c].Read(); System.out.printf("Qubit q%d = "+q[c], c);
+					switch(cmd[2]){
+					
+					case "pauli-x":
+						index = Integer.parseInt(cmd[3]);
+						q[index].cNOT(q[c]);
+						System.out.printf("Qubit q%d = "+q[index], index);
+						break;
+					
+					case "pauli-y":
+						index = Integer.parseInt(cmd[3]);
+						q[index].cY(q[c]);
+						System.out.printf("Qubit q%d = "+q[index], index);
+						break;
+					
+					case "pauli-z":
+						index = Integer.parseInt(cmd[3]);
+						q[index].cZ(q[c]);
+						System.out.printf("Qubit q%d = "+q[index], index);
+						break;
+					
+					case "hadamard":
+						index = Integer.parseInt(cmd[3]);
+						q[index].cHadamard(q[c]);
+						System.out.printf("Qubit q%d = "+q[index], index);
+						break;
+					
+					case "read":
+						index = Integer.parseInt(cmd[3]);
+						q[index].cRead(q[c]);
+						System.out.printf("Qubit q%d = "+q[index], index);
+						break;
+					
+					case "swap":
+						index = Integer.parseInt(cmd[3]);
+						index2 = Integer.parseInt(cmd[4]);
+						q[index].cSwap(q[index2], q[c]);
+						System.out.printf("Qubit q%d = "+q[index], index);
+						System.out.printf("Qubit q%d = "+q[index2], index2);
+						break;
+					
+					}
+					break;
+					
+				case "exit":
+					System.exit(0);
+					break;
+			}
+			
+		}
+	}
+}
+	
